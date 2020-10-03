50ba29de6e9ae77691e4899d501b8aa14d1cb096
MustafaYavas <44411206+MustafaYavas@users.noreply.github.com>
2020-10-03 14:01:43 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 // express'in yapılandırması için  parçaları tanımladık
 const publicDirectoryPath = path.join(__dirname,"../public");
@@ -103,6 +104,6 @@ app.get("*", (req,res) => {  //girilen tüm hatalı url için gösterilecek sayf
     })
 })
  
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");        
+app.listen(port, () => {
+    console.log("Server is up on port" + port);        
 })
